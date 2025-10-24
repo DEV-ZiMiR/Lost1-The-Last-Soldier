85199fbe219f8a242fd521aef56dd8112ad1ab31
ZiMiR <gamedevzimir@gmail.com>
2025-10-24 12:58:17 +0300

    Delete scripts directory

--- scripts/button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	pressed.connect(_on_pressed)
-
-func _on_pressed():
-	var inventory_ui = get_node("../../")  # або "../.." якщо ти всередині Panel
-	inventory_ui.visible = false
-	$"../../ItemContextMenu".visible = false
