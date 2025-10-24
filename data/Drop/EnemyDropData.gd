0b60dc153089493f9c504352e467689b07fa9117
ZiMiR <gamedevzimir@gmail.com>
2025-10-24 12:57:31 +0300

    Delete data directory

--- data/Drop/EnemyDropData.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name EnemyDropData
-
-@export_subgroup("Drop Settings")
-@export var drop_entries: Array[DropEntry] = []
--- data/InventorySlot.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name InventorySlot
-
-@export var item_data: ItemData
-@export var quantity: int = 1
